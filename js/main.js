89d98fd5ce3d852c2eddc68ec753027fc8a55d87
Kelly Tan <kellytan@seas.upenn.edu>
2017-02-04 22:49:22 -0500

    webvr

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ if (navigator.getVRDisplays) {
 }
 
 let mountain = new Mountain(7500, 7500, 24, 24);
+mountain.mesh.rotation.z = Math.PI/2;
 scene.add(mountain.mesh);
 
 let fireworkColors = [0xffffff, 0xff2b2b, 0xff8421, 0xffe41e, 0x65ff1e,
